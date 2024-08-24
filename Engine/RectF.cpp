8f05ea205febb89233f05b29b8a64ff1c285b2f4
MaxSush <psushant880@gmail.com>
2024-08-24 18:59:27 +0530

    added RectF class

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -1,1 +1,28 @@
 #include "RectF.h"
+
+RectF::RectF(float left, float top, float rigth, float bottom)
+	:
+	top(top),
+	left(left),
+	rigth(rigth),
+	bottom(bottom)
+{
+}
+
+RectF::RectF(const Vec2& top_left, const Vec2& bottom_rigth)
+	:
+	RectF(top_left.x, top_left.y, bottom_rigth.x, bottom_rigth.y)
+{
+}
+
+RectF::RectF(const Vec2& top_left, float width, float heigth)
+	:
+	RectF(top_left.x, top_left.y, width, heigth)
+{
+}
+
+bool RectF::isOverlappingWith(const RectF& other) const
+{
+	return left >= other.left && rigth <= other.rigth &&
+		top >= other.top && bottom <= other.bottom;
+}
--- Engine/RectF.h
+++ Engine/RectF.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "Vec2.h"
 class RectF
 {
+public:
+	RectF(float left, float top, float rigth, float bootom);
+	RectF(const Vec2& top_left, const Vec2& bottom_rigth);
+	RectF(const Vec2& top_left, float width, float heigth);
+	bool isOverlappingWith(const RectF& other) const;
+private:
+	float top;
+	float rigth;
+	float left;
+	float bottom;
 };
 
